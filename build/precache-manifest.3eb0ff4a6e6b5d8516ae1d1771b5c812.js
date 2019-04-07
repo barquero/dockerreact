fbd7abcb3def6e110ac034a0a56cddb311a3ec9e
Henrique Barquero <barquero@MacBook-Pro-de-Henrique.local>
2019-04-07 21:03:15 +0100

    initial

---
+++ build/precache-manifest.3eb0ff4a6e6b5d8516ae1d1771b5c812.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5235f8ba0a1b6c824ceb",
+    "url": "/static/js/main.e749dc36.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "5235f8ba0a1b6c824ceb",
+    "url": "/static/css/main.5e4638cc.chunk.css"
+  },
+  {
+    "revision": "2c71527b5beb96cca635a519f8cf6589",
+    "url": "/index.html"
+  }
+];
